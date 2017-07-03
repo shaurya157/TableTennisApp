fbddbd39aac85725d6a22b4cebd78f041c2fbbfc
Shaurya Sinha <shaurya157@gmail.com>
2017-07-03 23:13:54 +0800

    user model done and tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,33 @@
 class User < ApplicationRecord
+  validates :email, :password_digest, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  validates :email, uniqueness: true
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  has_many :followers, through: :user_follows, source: :follower
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
 end
