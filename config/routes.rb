39c99101fd84f975dec8990d237a9b8d187b7c04
Shaurya Sinha <shaurya157@gmail.com>
2017-07-04 12:46:07 +0800

    added routes for user and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show] do
+      member do
+        post 'follow'
+        delete 'unfollow'
+      end
+    end
+    
+    resource :session, only: [:create, :destroy]
+  end
 end
