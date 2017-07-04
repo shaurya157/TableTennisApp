c261310b1ddee463c48aafd3d7b0bedb91c1292c
Shaurya Sinha <shaurya157@gmail.com>
2017-07-04 12:39:56 +0800

    finished session controller

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,2 +1,27 @@
 class Api::SessionController < ApplicationController
+  def create
+  @user = User.find_by_credentials(session_params[:username], session_params[:password])
+
+  if @user
+    login!(@user)
+    render 'api/users/show'
+  else
+    render json: ['Invalid credentials'], status: 401
+  end
+end
+
+def destroy
+  if current_user
+    logout!
+    render json: {}
+  else
+    render json: ['No current user'], status: 404
+  end
+end
+
+private
+
+def session_params
+  params.require(:user).permit([:username, :password])
+end
 end
